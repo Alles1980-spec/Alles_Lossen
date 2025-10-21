4a3e6ac0f7984aecdfe484052b4e36e45e15850c
Alles1980-spec <nguwawor1980@gmail.com>
2025-10-21 13:18:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+const tombolSapa = document.getElemenByld('sapaButton')
+tombolSapa.addEventListener('click', function(){
+  alert('Halo ! Terima Kasih Atas Kunjunganmu ! Bagaimana Kabarmu Hari Ini ?');});
